47d55b75666134ddf597e47e57ccade3b2338f55
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2020-10-22 20:24:24 +0530

    Delete script.js

--- IT-Services/script.js
+++
@@ -1,6 +0,0 @@
-var hammenu = document.querySelector('#ham-menu');
-var hamitems = document.querySelector('#ham-items');
-
-hamburger.addEventListener('click', () => {
-    hamitems.classList.toggle("is-active");
-})
